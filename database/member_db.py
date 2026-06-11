d28a7249bded2064e43e723aedb7c0f06761eb29
Shlomi <shlomikaufman91@gmail.com>
2026-06-11 15:50:03 +0300

    Add get all members method

--- database/member_db.py
+++ database/member_db.py
@@ -11,6 +11,15 @@ class MemberDB:
 
     def craete_member(self, data):
         pass
+
+    def get_all_members(self):
+        logger.info("Start... Get all members from database")
+
+        self.cursor.execute("SELECT * FROM members")
+        rows = self.cursor.fetchall()
+        self.close()
+
+        return rows
     
     def close(self):
         self.conn.close()
