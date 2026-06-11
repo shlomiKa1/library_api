ad0657e54537a970494e90466869b56488447b92
Shlomi <shlomikaufman91@gmail.com>
2026-06-11 12:44:45 +0300

    Fix connection to a class

--- database/db_connection.py
+++ database/db_connection.py
@@ -3,63 +3,77 @@ from secret_details import CONMNECTION, DATABASE
 import mysql.connector
 
 
-def get_connection():
-    return mysql.connector.connect(
-        host=CONMNECTION["host"],
-        user=CONMNECTION["user"],
-        password=CONMNECTION["password"]
-    )
+class DbConn:
+    def __init__(self, host, user, password):
+        self.host = host,
+        self.user = user,
+        self.password = password
 
-def create_database():
-    conn = get_connection()
-    cursor = conn.cursor()
+        self.conn = self.get_connect()
 
-    cursor.execute("CREATE DATABASE IF NOT EXISTS %s", (DATABASE,))
-    logger.info("Database '%s' created successfully", DATABASE)
-    conn.commit()
-    
-    cursor.close()
-    conn.close()
+    def get_connect(self):
+        return mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password
+        )
 
-def create_table_books():
-    conn = get_connection()
-    cursor = conn.cursor()
+    def get_connection(self):
+        if self.conn.is_connected():
+            return self.conn
+        
+        self.conn = self.get_connect()
+        return self.conn
 
-    cursor.execute(
-        """
-            CREATE TABLE IF NOT EXISTS books(
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                title VARCHAR(50) NOT NULL,
-                author VARCHAR(50) NOT NULL,
-                genre ENUM(Fiction, Non-Fiction, Science, History, Other),
-                is_available BOOLEAN DEFUALT TRUE,
-                borrowed_by_member_id INT DEFUALT NULL
-            )
-        """
-    )
-    conn.commit()
-    logger.info("Books table created successfully")
-    
-    cursor.close()
-    conn.close()
+    def create_database(self):
+        cursor = self.conn.cursor()
 
-def create_table_members():
-    conn = get_connection()
-    cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS %s", (DATABASE,))
+        logger.info("Database '%s' created successfully", DATABASE)
+        self.conn.commit()
+        
+        cursor.close()
+        self.conn.close()
 
-    cursor.execute(
-        """
-            CREATE TABLE IF NOT EXISTS members(
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(50) NOT NULL,
-                email VARCHAR(30) UNIQUE,
-                is_active BOOLEAN DEFUALT TRUE,
-                total_borrows INT DEFUALT NULL
-            )
-        """
-    )
-    conn.commit()
-    logger.info("Members table created successfully")
+    def create_table_books(self):
+        cursor = self.conn.cursor()
 
-    cursor.close()
-    conn.close()
+        cursor.execute(
+            """
+                CREATE TABLE IF NOT EXISTS books(
+                    id INT PRIMARY KEY AUTO_INCREMENT,
+                    title VARCHAR(50) NOT NULL,
+                    author VARCHAR(50) NOT NULL,
+                    genre ENUM(Fiction, Non-Fiction, Science, History, Other),
+                    is_available BOOLEAN DEFUALT TRUE,
+                    borrowed_by_member_id INT DEFUALT NULL
+                )
+            """
+        )
+        self.conn.commit()
+        logger.info("Books table created successfully")
+        
+        cursor.close()
+        self.conn.close()
+
+    def create_table_members(self):
+        cursor = self.conn.cursor()
+
+        cursor.execute(
+            """
+                CREATE TABLE IF NOT EXISTS members(
+                    id INT PRIMARY KEY AUTO_INCREMENT,
+                    name VARCHAR(50) NOT NULL,
+                    email VARCHAR(30) UNIQUE,
+                    is_active BOOLEAN DEFUALT TRUE,
+                    total_borrows INT DEFUALT NULL
+                )
+            """
+        )
+        self.conn.commit()
+        logger.info("Members table created successfully")
+
+        cursor.close()
+        self.conn.close()
+
+db = DbConn(CONMNECTION["host"], CONMNECTION["user"], CONMNECTION["password"])
